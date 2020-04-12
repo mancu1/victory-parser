11663d8ccf26ed71e245caed6723416d530dfd9b
Mansur Zakirov <mansur.zakirov@boolqa.com>
2020-04-12 21:09:34 +0300

    = edit proxy env

--- src/telegramBot/initBot.ts
+++ src/telegramBot/initBot.ts
@@ -3,9 +3,15 @@ import socksAgent from "../proxy/proxy";
 import { Agent } from "https";
 import dotenv from "dotenv";
 dotenv.config();
-const bot = new Telegraf(process.env.BOT_TOKEN, {
-  telegram: {
-    agent: (socksAgent as unknown) as Agent,
-  },
-});
+
+const option =
+  process.env.USE_PROXY === "true"
+    ? {
+        telegram: {
+          agent: (socksAgent as unknown) as Agent,
+        },
+      }
+    : null;
+
+const bot = new Telegraf(process.env.BOT_TOKEN, option);
 export default bot;
